9a20f7069dcfed09a953a20ea3dc88aa01cf919d
daman0013 <daman000013@gmail.com>
2023-02-21 10:31:56 +0530

    n

---
+++ pages/layout/layout.js
@@ -0,0 +1,11 @@
+export default function Layout({children}){
+    return(
+        <div className="flex h-screen bg-blue-400">
+            <div>
+                
+            </div>
+            
+            {children}
+        </div>
+    )
+}
